64bbcbcc5855f293bbf2f68e4abb1b975919030f
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-01-12 06:16:32 +0200

    Day10

---
+++ 389-Find_the_Difference.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            for i in t:
+                if t.count(i) > s.count(i):
+                    return(i)
+                    break
