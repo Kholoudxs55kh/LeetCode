d4dc859ee35f09181a05370198b8eadc88fb15c5
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-01-12 06:40:14 +0200

    Day11 - can't believe i did that to myself

---
+++ 58-Length_of_Last_Word.c
@@ -0,0 +1,15 @@
+int lengthOfLastWord(char* s) {
+    char *vector = strtok(s, " ");
+    char *lastWord;
+
+    while (vector) {
+        lastWord = vector;
+        vector = strtok(NULL, " ");
+    };
+
+    if (lastWord) {
+        return strlen(lastWord);
+    };
+
+    return 0;
+}
