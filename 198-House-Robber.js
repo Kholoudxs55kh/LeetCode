58236738d44234270eef1247a798a11cc255764e
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-01-30 21:17:10 +0200

    [198] [Time Beats: 25.83%] [Memory Beats: 76.36%] - LeetPush

--- 198-House-Robber.js
+++ 198-House-Robber.js
@@ -1,18 +1,1 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var rob = function(nums) {
-    const robbedHouses = [nums[0]];
-    const skippedHouses = [0]; 
-
-    if (nums.length === 0) return 0;
-
-    for (let i = 1; i < nums.length; i++) {
-        robbedHouses[i] = skippedHouses[i - 1] + nums[i];
-        skippedHouses[i] = Math.max(robbedHouses[i - 1], skippedHouses[i - 1]);
-    }
-    const max = Math.max(robbedHouses[nums.length - 1], skippedHouses[nums.length - 1])
-
-    return max;
-};
+null
