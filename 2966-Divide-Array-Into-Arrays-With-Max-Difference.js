13332d483cfd04ee64d1af83b10fe5e237099fe9
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-02-01 15:40:52 +0200

    [2966] [Time Beats: 92.06%] [Memory Beats: 34.60%] - LeetPush

--- 2966-Divide-Array-Into-Arrays-With-Max-Difference.js
+++ 2966-Divide-Array-Into-Arrays-With-Max-Difference.js
@@ -1,1 +1,14 @@
-null
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[][]}
+ */
+var divideArray = function(nums, k) {
+    nums.sort((a, b) => a - b);
+    let output = [];
+    for (let i = 2; i < nums.length; i += 3) {
+        if (nums[i] - nums[i - 2] > k) return [];
+        output.push([nums[i - 2], nums[i - 1], nums[i]]);
+    }
+    return output;
+};
