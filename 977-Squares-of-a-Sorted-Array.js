46b559f0a37af88cab4ecc4cfdcb27862c75f707
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-03-02 14:09:11 +0200

    [977] [Time Beats: 42.12%] [Memory Beats: 41.04%] - LeetPush

---
+++ 977-Squares-of-a-Sorted-Array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+
+    const arr = []
+
+    for (let i = 0 ; i < nums.length; i++) {
+        arr.push(nums[i]**2)
+    }
+
+    return arr.sort((a, b) => a - b);
+
+};
