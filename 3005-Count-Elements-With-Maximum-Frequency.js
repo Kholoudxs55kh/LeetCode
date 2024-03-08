17df0b35bb9c7d9150527e28748d5e993619be36
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-03-08 20:14:39 +0200

    [3005] [Time Beats: 12.67%] [Memory Beats: 68.23%] - LeetPush

---
+++ 3005-Count-Elements-With-Maximum-Frequency.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxFrequencyElements = function(nums) {
+    const freq = new Map();
+
+    for (const num of nums) {
+        freq.set(num, (freq.get(num) || 0) + 1);
+    }
+
+    const maxFreq = Math.max(...freq.values());
+
+    const maxFreqEle = [...freq.keys()].filter(num => freq.get(num) === maxFreq);
+
+    return maxFreq * maxFreqEle.length;
+};
