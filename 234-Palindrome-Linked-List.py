984151b78f3f65f219901081f6799c29c4f7550a
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-03-23 00:10:22 +0200

    [234] [Time Beats: 14.43%] [Memory Beats: 86.44%] - LeetPush

---
+++ 234-Palindrome-Linked-List.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        cur = head
+        nums = list()
+        while cur:
+            nums.append(cur.val)
+            cur = cur.next
+        return nums == nums[::-1]     
