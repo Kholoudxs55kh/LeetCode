ae3f066aeeaccb81259a70330096502dc1a4067c
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-04-13 18:00:58 +0200

    [85] [Time Beats: 25.31%] [Memory Beats: 96.91%] - LeetPush

---
+++ 85-Maximal-Rectangle.js
@@ -0,0 +1,37 @@
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalRectangle = function(matrix) {
+    let m = matrix.length,
+        n = matrix[0].length;
+
+    let widthOnLeftTillHere = Array.from({ length: m }, () =>
+        new Array(n).fill(0)
+    );
+    for (let r = 0; r < m; r++)
+        for (let c = 0; c < n; c++)
+            if (matrix[r][c] === "1") {
+                widthOnLeftTillHere[r][c] =
+                    (widthOnLeftTillHere[r][c - 1] || 0) + 1;
+            }
+
+    let maxArea = 0;
+    for (let r = 0; r < m; r++)
+        for (let c = 0; c < n; c++)
+            if (matrix[r][c] === "1") {
+                let minWidthGoingUp = Infinity;
+                let increasingHeight = 1;
+                for (let r2 = r; r2 >= 0; r2--, increasingHeight++) {
+                    minWidthGoingUp = Math.min(
+                        minWidthGoingUp,
+                        widthOnLeftTillHere[r2][c]
+                    );
+                    maxArea = Math.max(
+                        maxArea,
+                        minWidthGoingUp * increasingHeight
+                    );
+                }
+            }
+    return maxArea;
+};
