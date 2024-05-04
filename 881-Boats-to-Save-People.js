0d0c7c0d1c4e80f406918724c14310aa00feaeab
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-05-04 23:50:14 +0300

    [881] [Time Beats: 99.13%] [Memory Beats: 18.95%] - LeetPush

---
+++ 881-Boats-to-Save-People.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function(people, limit) {
+    people.sort((a, b) => a - b);
+    console.log(people)
+    let count = 0;
+    let left = 0;
+    let right = people.length - 1;
+
+    while (left <= right) {
+        if (people[left] + people[right] <= limit) left++;
+        right--;
+        count++;
+    }
+
+    return count;
+};
