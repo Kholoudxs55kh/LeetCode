bceab17dc50e1d9bca0a88f2a623fb698224cefe
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-06-13 23:51:07 +0300

    [1913] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

--- 1913-Maximum-Product-Difference-Between-Two-Pairs.js
+++ 1913-Maximum-Product-Difference-Between-Two-Pairs.js
@@ -3,12 +3,6 @@
  * @return {number}
  */
 var maxProductDifference = function(nums) {
-    if (!nums.length || nums.length < 3) return 0
-
-    nums.sort((a,b) => a - b)
-
-    let first = nums[0] * nums[1]
-    let second = nums[nums.length - 1] * nums[nums.length - 2]
-
-    return second - first
+    nums = nums.sort((a, b)=> a - b)
+    return Math.abs((nums[1] * nums[0]) - (nums[nums.length - 1] * nums[nums.length - 2]))
 };
