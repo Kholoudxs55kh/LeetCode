b33ca242d39917c9221943a727dfbed83bc6233e
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-12-26 23:03:59 +0200

    [242] [Time Beats: 71.43%] [Memory Beats: 90.97%] - LeetPush

--- 242-Valid-Anagram.js
+++ 242-Valid-Anagram.js
@@ -3,15 +3,28 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
+var isAnagram = function (s, t) {
+    /**
+    - the anagram is to have 2 words containing the same chars \count and char\
+    
+    - edge case: check their length is !== return false
+    - edge case: check the string if it has a non-char
+    
+    - how to solve it
+        - https://discord.com/channels/1319972809950232606/1321923765612838922/1321941535926784020
+     */
+
     if (s.length !== t.length) return false
 
-    const sortedS = Array.from(s).sort().join("")
-    const sortedT = Array.from(t).sort().join("")
+    let hash = {}
 
-    for (let i = 0;i < s.length; i++) {
-        if (sortedS[i] !== sortedT[i]) return false
+    for (let i = 0; i < s.length; i++) {
+        hash[s[i]] ? hash[s[i]] += 1 : hash[s[i]] = 1
+        hash[t[i]] ? hash[t[i]] -= 1 : hash[t[i]] = -1;
     }
-
-    return true
+    
+    for (let key in hash) {
+        if (hash[key] !== 0) return false;
+    }
+    return true;
 };
