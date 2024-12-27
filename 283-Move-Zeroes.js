f6c7622aa6e1b2f56737bd2b86ef78cf930f89f0
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-12-27 22:56:55 +0200

    [283] [Time Beats: 100.00%] [Memory Beats: 61.36%] - LeetPush

---
+++ 283-Move-Zeroes.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let notZero = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[notZero] = nums[i];
+            notZero += 1;
+        }
+    }
+    for (let i = notZero; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+};
