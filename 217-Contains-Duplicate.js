423ec0013780b87fcba5577856f7a9d2db7ca7f4
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-12-28 22:40:07 +0200

    [217] [Time Beats: 39.56%] [Memory Beats: 52.06%] - LeetPush

--- 217-Contains-Duplicate.js
+++ 217-Contains-Duplicate.js
@@ -2,6 +2,22 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    return (new Set(nums).size) !== nums.length 
+var containsDuplicate = function (nums) {
+    /**
+    main target => check if avalue is repeated
+
+    [1,2,3,1]
+    - store every ele as a key and a val of 1 ||| karem's idea
+    - check the next ele to find if it is repeated
+    - if repeated increase its val
+    - check the val if any key has any BUT 1 return true
+    - if okay return false
+     */
+    const countMap = {};
+    for (let num of nums) {
+        if (countMap[num]) return true;
+        countMap[num] = 1;
+    }
+
+    return false;
 };
