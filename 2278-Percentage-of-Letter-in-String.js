ec26a3f0b7ec633ceaa4f8992d0c35c09536473b
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-12-31 22:31:37 +0200

    [2278] [Time Beats: 100.00%] [Memory Beats: 81.55%] - LeetPush

---
+++ 2278-Percentage-of-Letter-in-String.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {character} letter
+ * @return {number}
+ */
+var percentageLetter = function(s, letter) {
+    /**
+    - Idea:
+
+    - Edge Cases:
+
+    - Approach:
+     */
+
+     let counter = 0
+
+     for (let i = 0; i < s.length; i++) {
+        if (s[i] === letter) counter++
+     }
+
+     return Math.floor((counter / s.length) * 100)
+};
