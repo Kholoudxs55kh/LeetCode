e9843d7f9ca003148e0c527fa893aff4ca3c9021
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-01-02 22:52:05 +0200

    [3216] [Time Beats: 95.56%] [Memory Beats: 84.44%] - LeetPush

--- 3216-Lexicographically-Smallest-String-After-a-Swap.js
+++ 3216-Lexicographically-Smallest-String-After-a-Swap.js
@@ -3,6 +3,19 @@
  * @return {string}
  */
 const getSmallestString = (s) => {
+    /**
+    Idea:
+        - so we will swap at most once so we can get the possible smallest number we can get.
+    
+    Edge Cases: NAN
+        
+    Approach: 
+            1- iterate over string
+            2- check parity of two adjacent number
+            3- if true then swap if the first number is bigger than the second one then break
+            4- else repeat for the next adjacent numbers
+            5- print the output
+    */
   const digits = [...s];
   for (let i = 1; i < digits.length; i++) {
     if (digits[i - 1] > digits[i] && (digits[i - 1] % 2 === digits[i] % 2)) {
