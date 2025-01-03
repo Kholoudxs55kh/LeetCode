06bb79695790f4f70b57eba78f737984ed1175d2
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-01-03 23:34:45 +0200

    [539] [Time Beats: 98.10%] [Memory Beats: 100.00%] - LeetPush

--- 539-Minimum-Time-Difference.js
+++ 539-Minimum-Time-Difference.js
@@ -23,34 +23,52 @@ var findMinDifference = function (timePoints) {
     const set = new Set(timePoints)
     if (set.size !== timePoints.length) return 0
 
-    let arrOfTimes = []
+    // let arrOfTimes = []
 
-    for (let i = 0; i < timePoints.length; i++) {
-        // if (timePoints[i] === \00:00\) {
-        //     arrOfTimes.push(24 * 60)
-        // }
+    // for (let i = 0; i < timePoints.length; i++) {
+    //     // if (timePoints[i] === \00:00\) {
+    //     //     arrOfTimes.push(24 * 60)
+    //     // }
 
-        let vals = timePoints[i].split(\:\)
-        let mnts = (parseInt(vals[0]) * 60) + parseInt(vals[1])
+    //     let vals = timePoints[i].split(\:\)
+    //     let mnts = (parseInt(vals[0]) * 60) + parseInt(vals[1])
 
-        arrOfTimes.push(mnts)
+    //     arrOfTimes.push(mnts)
+    // }
+
+    // arrOfTimes.sort((a, b) => a - b)
+    // console.log(arrOfTimes)
+
+    // let minDiff = Infinity
+
+    // for (let i = 0; i < arrOfTimes.length - 1; i++) {
+    //     console.log(i)
+    //     let diff = arrOfTimes[i + 1] - arrOfTimes[i]
+
+    //     console.log(diff)
+    //     if (minDiff > diff) minDiff = diff
+    // }
+
+    // let circularDiff = (1440 - arrOfTimes[arrOfTimes.length - 1]) + arrOfTimes[0]
+
+    // minDiff = Math.min(minDiff, circularDiff)
+    // return minDiff
+
+
+    // ======== Optimized using gpt ========
+    const minutes = timePoints.map(time => {
+        const [hours, mins] = time.split(\:\).map(Number);
+        return hours * 60 + mins;
+    }).sort((a, b) => a - b);
+
+    let minDiff = Infinity;
+
+    for (let i = 1; i < minutes.length; i++) {
+        minDiff = Math.min(minDiff, minutes[i] - minutes[i - 1]);
     }
 
-    arrOfTimes.sort((a, b) => a - b)
-    console.log(arrOfTimes)
+    const circularDiff = 1440 - minutes[minutes.length - 1] + minutes[0];
+    minDiff = Math.min(minDiff, circularDiff);
 
-    let minDiff = Infinity
-
-    for (let i = 0; i < arrOfTimes.length - 1; i++) {
-        console.log(i)
-        let diff = arrOfTimes[i + 1] - arrOfTimes[i]
-
-        console.log(diff)
-        if (minDiff > diff) minDiff = diff
-    }
-
-    let circularDiff = (1440 - arrOfTimes[arrOfTimes.length - 1]) + arrOfTimes[0]
-
-    minDiff = Math.min(minDiff, circularDiff)
-    return minDiff
+    return minDiff;
 };
