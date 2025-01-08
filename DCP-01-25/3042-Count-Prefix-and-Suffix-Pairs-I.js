ee8ff683bbd51d2cae71d2da1cdaa2683fce577f
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-01-08 23:47:00 +0200

    [3042] [Time Beats: 60.64%] [Memory Beats: 89.36%] - LeetPush

--- DCP-01-25/3042-Count-Prefix-and-Suffix-Pairs-I.js
+++ DCP-01-25/3042-Count-Prefix-and-Suffix-Pairs-I.js
@@ -4,11 +4,22 @@
  */
 var countPrefixSuffixPairs = function (words) {
     /**
-    Idea:
-    
-    Edge Cases:
-
+       Idea:
+        -- compare the str1 with the start and the end of the other strings 
+        -- index of str1 < index of str2
+        -- if applicable return true else return false
+        -- return the count applicable pairs
+    Edge case:
+        -- length of words = 1 => return  0
+        -- if str2.length < str1.length => return false
     Approach:
+        -- iterate over the words array i = 0 till i < words.length
+        -- iterate over the rest of the array
+        -- check the result of the condition of prefix and suffix
+        -- mainpulate the counter and return it 
+        -- In isPrefixAndSuffix function check the condition
+        -- if the condition true increment counter 
+        -- return count
      */
 
     function isPrefixAndSuffix(str1, str2) {
