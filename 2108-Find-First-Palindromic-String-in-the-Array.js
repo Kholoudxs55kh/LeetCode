0a03676019729d0f9405ea7504c4dc7a8eb8b3ed
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-01-15 22:33:14 +0200

    [2108] [Time Beats: 92.44%] [Memory Beats: 74.79%] - LeetPush

--- 2108-Find-First-Palindromic-String-in-the-Array.js
+++ 2108-Find-First-Palindromic-String-in-the-Array.js
@@ -2,11 +2,33 @@
  * @param {string[]} words
  * @return {string}
  */
-var firstPalindrome = function(words) {
-    for (word of words) {
-        // console.log(Array.from(word))
-        // console.log(Array.from(word).reverse())
-        if (word ===  word.split('').reverse().join('')) return word
+var firstPalindrome = function (words) {
+    /**
+    Idea:
+        -- return the first palindrome to meet or ""
+
+    Edge Cases:
+        -- N/A
+
+    Approach:
+        -- loop over the array, check every string and return the first to meet as palindrome
+    */
+
+    // for (let word of words) {
+    //     if (word === word.split('').reverse().join('')) return word
+    // }
+
+    for (let word of words) {
+        let isPal = true;
+
+        for (let i = 0, j = word.length - 1; i < word.length, j >= 0; i++, j--) {
+            if (word[i] !== word[j]) {
+                isPal = false
+                break
+            }
+        }
+
+        if (isPal) return word
     }
 
     return ""
