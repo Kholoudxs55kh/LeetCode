8b8ba2ea0aaa49efc0a240f9ef2161fb11d08c55
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-01-21 00:23:20 +0200

    [26] [Time Beats: 5.02%] [Memory Beats: 5.21%] - LeetPush

--- 26-Remove-Duplicates-from-Sorted-Array.js
+++ 26-Remove-Duplicates-from-Sorted-Array.js
@@ -3,13 +3,23 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    let i = 0, j = 1
-    for (; i <= j, j < nums.length;) {
-        if (nums[i] === nums[j]) j++
-        else {
-            nums[i + 1] = nums[j]
-            i++
+    /**
+
+    idea: return the number of unique elements \k\ and modify the nums array with the first \k\ elements are unique
+
+    edge cases: if nums length equal 1 return nums length
+
+    approach: 
+        up to us
+
+    */
+    let count = 1
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] !== nums[i + 1]) {
+            nums[count] = nums[i + 1]
+            console.log(nums)
+            count++
         }
     }
-    return i + 1
+    return count
 };
