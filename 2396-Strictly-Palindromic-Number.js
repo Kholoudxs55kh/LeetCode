91603033deabfd5f9b26d5ba4f78c3d365969739
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-01-21 22:23:02 +0200

    [2396] [Time Beats: 100.00%] [Memory Beats: 37.98%] - LeetPush

---
+++ 2396-Strictly-Palindromic-Number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isStrictlyPalindromic = function(n) {
+    /**
+    Idea:
+
+    Edge Cases:
+
+    Approach:
+    */
+
+    return n === 3
+};
