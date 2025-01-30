10b29d05660ffc22f962508a82fee0c36e7d4f1d
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-01-30 22:19:59 +0200

    [2937] [Time Beats: 66.67%] [Memory Beats: 80.77%] - LeetPush

---
+++ 2937-Make-Three-Strings-Equal.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} s3
+ * @return {number}
+ */
+var findMinimumOperations = function (s1, s2, s3) {
+    let count = 0
+
+    for (let i = 0; i < s1.length; i++) {
+        if (s1[i] !== s2[i] || s1[i] !== s3[i]) break
+        
+        count++
+    }
+
+    return count === 0 ? -1 : (s1.length + s2.length + s3.length) - (3 * count)
+
+};
