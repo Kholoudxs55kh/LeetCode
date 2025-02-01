de6c7319f629994c781d612dbeaef26fdfa3a6c2
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-02-01 22:29:10 +0200

    [3151] [Time Beats: 100.00%] [Memory Beats: 29.11%] - LeetPush

--- DCP-02-25/3151-Special-Array-I.js
+++ DCP-02-25/3151-Special-Array-I.js
@@ -3,8 +3,20 @@
  * @return {boolean}
  */
 var isArraySpecial = function (nums) {
-
-    for (let i = 1; i < nums.length; i++) if (nums[i - 1] % 2 === nums[i] % 2) return false
+    /**
+    Idea:
+        even odd even odd => true
+        even even => false
+        odd odd  => false
     
+    Edge Cases:
+        N/A
+
+    Approach:
+        iterate over the array and check the elemet and what's next or prev, if both even or odd return false, else return true
+    */
+
+    for (let i = 1; i < nums.length; i++) if (nums[i] % 2 === nums[i - 1] % 2) return false
+
     return true
-};
+}
