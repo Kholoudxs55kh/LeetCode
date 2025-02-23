cf1acd6b9c16b91d48db12f0cdaa854cbedca6b7
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-02-23 21:36:16 +0200

    [2506] [Time Beats: 74.82%] [Memory Beats: 5.04%] - LeetPush

---
+++ 2506-Count-Pairs-Of-Similar-Strings.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var similarPairs = function (words) {
+    words = words.map((curr) => {
+        let set = new Set([...curr])
+        let arr = Array.from(set)
+        arr.sort((a, b) => {
+            if (a < b) return -1
+            else return 1
+        })
+        return arr.join('')
+    })
+
+    let pairs = words.reduce((map, cur) => map.set(cur, map.get(cur) + 1 || 1), new Map())
+
+    let count = 0
+    for (let pair of pairs.values()) count += (pair * (pair - 1)) / 2
+
+    return count
+};
