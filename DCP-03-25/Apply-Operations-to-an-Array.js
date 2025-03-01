c00e9a8e2f3c604e8236533a031c04435e26a658
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-03-01 19:24:12 +0200

    [2460] [Time Beats: 100.00%] [Memory Beats: 5.39%] - LeetPush

---
+++ DCP-03-25/Apply-Operations-to-an-Array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var applyOperations = function(nums) {
+    let res = [];
+    for (let i = 0; i < nums.length - 1; i++) { 
+        if (nums[i] == nums[i + 1]) {
+            nums[i] = nums[i] * 2
+            nums[i + 1] = 0
+        }
+    }
+    for (let num of nums) if (num !== 0) res.push(num);
+    
+    while (res.length < nums.length) res.push(0);
+
+    return res;
+};
