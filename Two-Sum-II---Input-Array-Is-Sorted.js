9dc99ef94b335d9b97322c835edc92ad9aa00739
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-03-31 21:45:04 +0200

    [167] [Time Beats: 100.00%] [Memory Beats: 58.16%] - LeetPush

--- Two-Sum-II---Input-Array-Is-Sorted.js
+++ Two-Sum-II---Input-Array-Is-Sorted.js
@@ -4,15 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let low = 0;
-    let high = numbers.length - 1;
+    let low = 0
+    let high = numbers.length - 1
 
     while (low <= high) {
 
-        if (numbers[low] + numbers[high] > target) high--;
-        else if (numbers[low] + numbers[high] < target) low++;
-        else return [low + 1, high + 1];
+        if (numbers[low] + numbers[high] > target) high--
+        else if (numbers[low] + numbers[high] < target) low++
+        else return [low + 1, high + 1]
     }
     
-    return [];
+    return []
 };
