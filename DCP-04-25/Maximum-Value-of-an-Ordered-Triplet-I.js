74eca58bc917dce706e3e6ef1935bae6a2b22f8d
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-04-02 20:11:07 +0200

    [2873] [Time Beats: 12.16%] [Memory Beats: 40.54%] - LeetPush

---
+++ DCP-04-25/Maximum-Value-of-an-Ordered-Triplet-I.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumTripletValue = function (nums) {
+    let max = 0
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                let val = (nums[i] - nums[j]) * nums[k]
+                max = Math.max(val, max)
+            }
+        }
+    }
+    return max
+};
