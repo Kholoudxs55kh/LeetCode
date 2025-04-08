0696a62489cb0f5e2414d544798447ebc860cb75
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-04-08 22:30:54 +0200

    [3396] [Time Beats: 100.00%] [Memory Beats: 57.47%] - LeetPush

--- DCP-04-25/Minimum-Number-of-Operations-to-Make-Elements-in-Array-Distinct.js
+++ DCP-04-25/Minimum-Number-of-Operations-to-Make-Elements-in-Array-Distinct.js
@@ -3,6 +3,21 @@
  * @return {number}
  */
 var minimumOperations = function (nums) {
+    /**
+    Idea:
+        - we need to have an array that has all unique values by repeating an operation that delete the first 3 items.
+
+    Edge Cases:
+        - make sure the list doesn't contain unique elements
+
+    Approach:
+        - brute force approach 
+    1 - iterate over array every 3 steps till reaching for unique elements by iterating over the remaining part
+
+    *** another sol
+    iterate backward over array and check if the element we are over is distinct then if we found the element is not unique then will use its index to get the needed steps to do the operation 
+    */
+
     if (new Set(nums).size === nums.length) return 0
 
     const set = new Set()
