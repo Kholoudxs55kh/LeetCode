e8814bc514186ba4b6598db13aad19d5546354c5
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2025-04-11 23:20:59 +0200

    [2255] [Time Beats: 47.47%] [Memory Beats: 12.66%] - LeetPush

--- Count-Prefixes-of-a-Given-String.js
+++ Count-Prefixes-of-a-Given-String.js
@@ -3,12 +3,34 @@
  * @param {string} s
  * @return {number}
  */
-var countPrefixes = function(words, s) {
+var countPrefixes = function (words, s) {
+    /**
+    Idea: we have to check every item of the words if it represents a prefix of "S", count them and return the count
+    
+    Appraoch: 
+        itterate over the array and check if every item Could be a prefix of the given s or not
+
+        while itterating we have to keep an eye to count how many of them is valid
+
+        return the count after itteration
+     */
+
+    const isPrefix = (w) => {
+         for (let i = 0; i < w.length; i++) {
+            if (word[i] !== s[i]) return false
+        }
+        return true
+    }
+
     let count = 0
     for (word of words) {
-        if (s.startsWith(word)) {
-            count++;
-        }
+        // if (word.length > s.length) continue
+        // if (s.startsWith(word)) count++
+        if (isPrefix(word)) count++
     }
+
     return count
+
+    // Time Complexity = O(N*M)
+    // Space Complexity = O(1)
 };
